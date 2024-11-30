6378ae78ada5bbad10f56da9a6eff243a195f98c
Carlos <carloseliesersantos@gmail.com>
2024-11-30 15:32:36 -0500

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import { glyphMap } from "./glyphMap";
 import createIconSet from "@expo/vector-icons/createIconSet";
 import { fonts } from "../scripts/common";
+import { Icon } from "@expo/vector-icons/build/createIconSet";
 
-const exports = {};
+const exports: Record<string, Icon<keyof typeof glyphMap, string>> = {};
 
 fonts.forEach((fontName) => {
 	exports[fontName] = createIconSet(glyphMap, fontName, fontName + ".ttf");
 });
 
-export default exports;
+export const MaterialSymbols = exports["MaterialSymbolsOutlined"];
+export const MaterialSymbolsFilled = exports["MaterialSymbolsOutlinedFilled"];
+export const MaterialSymbolsOutlined = exports["MaterialSymbolsOutlined"];
+export const MaterialSymbolsOutlinedFilled = exports["MaterialSymbolsOutlinedFilled"];
+export const MaterialSymbolsRounded = exports["MaterialSymbolsRounded"];
+export const MaterialSymbolsRoundedFilled = exports["MaterialSymbolsRoundedFilled"];
+export const MaterialSymbolsSharp = exports["MaterialSymbolsSharp"];
+export const MaterialSymbolsSharpFilled = exports["MaterialSymbolsSharpFilled"];
