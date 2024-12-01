ab6fa0c65374f49ea5cfdf59507e5e3705aa038e
Carlos <carloseliesersantos@gmail.com>
2024-11-30 20:29:05 -0500

    refactor

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,36 @@ import { glyphMap } from "./glyphMap";
 import createIconSet from "@expo/vector-icons/createIconSet";
 import { Icon } from "@expo/vector-icons/build/createIconSet";
 
-const fonts = ["MaterialSymbolsOutlined", "MaterialSymbolsOutlinedFilled", "MaterialSymbolsRounded", "MaterialSymbolsRoundedFilled", "MaterialSymbolsSharp", "MaterialSymbolsSharpFilled"];
-const exports: Record<string, Icon<keyof typeof glyphMap, string>> = fonts.reduce((acc, fontName) => {
+const fonts = [
+	"MaterialSymbolsOutlined",
+	"MaterialSymbolsOutlinedFilled",
+	"MaterialSymbolsRounded",
+	"MaterialSymbolsRoundedFilled",
+	"MaterialSymbolsSharp",
+	"MaterialSymbolsSharpFilled",
+];
+
+const exports: Record<
+	string,
+	Icon<keyof typeof glyphMap, string>
+> = fonts.reduce((acc, fontName) => {
 	acc[fontName] = createIconSet(glyphMap, fontName, fontName + ".ttf");
 	return acc;
 }, {});
-const paths = fonts.map((fontName) => "node_modules/material-symbols-expo/dist/assets/fonts/" + fontName + ".ttf");
+
+const paths = fonts.map(
+	(fontName) =>
+		`node_modules/material-symbols-expo/dist/assets/fonts/${fontName}.ttf`,
+);
 
 export const MaterialSymbols = exports["MaterialSymbolsOutlined"];
 export const MaterialSymbolsFilled = exports["MaterialSymbolsOutlinedFilled"];
 export const MaterialSymbolsOutlined = exports["MaterialSymbolsOutlined"];
-export const MaterialSymbolsOutlinedFilled = exports["MaterialSymbolsOutlinedFilled"];
+export const MaterialSymbolsOutlinedFilled =
+	exports["MaterialSymbolsOutlinedFilled"];
 export const MaterialSymbolsRounded = exports["MaterialSymbolsRounded"];
-export const MaterialSymbolsRoundedFilled = exports["MaterialSymbolsRoundedFilled"];
+export const MaterialSymbolsRoundedFilled =
+	exports["MaterialSymbolsRoundedFilled"];
 export const MaterialSymbolsSharp = exports["MaterialSymbolsSharp"];
 export const MaterialSymbolsSharpFilled = exports["MaterialSymbolsSharpFilled"];
 export const MaterialSymbolsFonts = paths;
