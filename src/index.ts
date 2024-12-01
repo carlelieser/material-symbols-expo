3fa6c36d1500de950778455769b535754201baaa
Carlos <carloseliesersantos@gmail.com>
2024-11-30 20:20:18 -0500

    refactor

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import createIconSet from "@expo/vector-icons/createIconSet";
 import { Icon } from "@expo/vector-icons/build/createIconSet";
 
 const fonts = ["MaterialSymbolsOutlined", "MaterialSymbolsOutlinedFilled", "MaterialSymbolsRounded", "MaterialSymbolsRoundedFilled", "MaterialSymbolsSharp", "MaterialSymbolsSharpFilled"];
-const exports: Record<string, Icon<keyof typeof glyphMap, string>> = {};
-
-fonts.forEach((fontName) => {
-	exports[fontName] = createIconSet(glyphMap, fontName, fontName + ".ttf");
-});
+const exports: Record<string, Icon<keyof typeof glyphMap, string>> = fonts.reduce((acc, fontName) => {
+	acc[fontName] = createIconSet(glyphMap, fontName, fontName + ".ttf");
+	return acc;
+}, {});
+const paths = fonts.map((fontName) => "node_modules/material-symbols-expo/dist/assets/fonts/" + fontName + ".ttf");
 
 export const MaterialSymbols = exports["MaterialSymbolsOutlined"];
 export const MaterialSymbolsFilled = exports["MaterialSymbolsOutlinedFilled"];
@@ -17,4 +17,4 @@ export const MaterialSymbolsRounded = exports["MaterialSymbolsRounded"];
 export const MaterialSymbolsRoundedFilled = exports["MaterialSymbolsRoundedFilled"];
 export const MaterialSymbolsSharp = exports["MaterialSymbolsSharp"];
 export const MaterialSymbolsSharpFilled = exports["MaterialSymbolsSharpFilled"];
-export const MaterialSymbolsFonts = fonts.map((fontName) => "node_modules/material-symbols-expo/dist/assets/fonts/" + fontName + ".ttf");
+export const MaterialSymbolsFonts = paths;
