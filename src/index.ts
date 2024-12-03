5b3124887454d2acb4dd3bf9faddbe3ededd8e3f
Carlos <carloseliesersantos@gmail.com>
2024-12-03 16:14:12 -0500

    export font assets and paths individually and as maps

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,15 @@ const fonts = [
 	"MaterialSymbolsSharpFilled",
 ];
 
+const fontAssets = {
+	MaterialSymbolsOutlined: require("./assets/fonts/MaterialSymbolsOutlined.ttf"),
+	MaterialSymbolsOutlinedFilled: require("./assets/fonts/MaterialSymbolsOutlinedFilled.ttf"),
+	MaterialSymbolsRounded: require("./assets/fonts/MaterialSymbolsRounded.ttf"),
+	MaterialSymbolsRoundedFilled: require("./assets/fonts/MaterialSymbolsRoundedFilled.ttf"),
+	MaterialSymbolsSharp: require("./assets/fonts/MaterialSymbolsSharp.ttf"),
+	MaterialSymbolsSharpFilled: require("./assets/fonts/MaterialSymbolsSharpFilled.ttf"),
+};
+
 export const MaterialSymbolsIconSet = fonts.reduce(
 	(acc, fontName) => {
 		acc[fontName] = createIconSet(glyphMap, fontName, fontName);
@@ -27,6 +36,15 @@ const paths = fonts.map(
 		`node_modules/material-symbols-expo/dist/assets/fonts/${fontName}.ttf`,
 );
 
+const pathsMap = paths.reduce(
+	(acc, path, i) => {
+		const fontName = fonts[i];
+		acc[fontName] = path;
+		return acc;
+	},
+	{} as Record<keyof typeof fontAssets, string>,
+);
+
 export const MaterialSymbolsOutlined =
 	MaterialSymbolsIconSet.MaterialSymbolsOutlined;
 export const MaterialSymbolsOutlinedFilled =
@@ -38,6 +56,19 @@ export const MaterialSymbolsRoundedFilled =
 export const MaterialSymbolsSharp = MaterialSymbolsIconSet.MaterialSymbolsSharp;
 export const MaterialSymbolsSharpFilled =
 	MaterialSymbolsIconSet.MaterialSymbolsSharpFilled;
-export const MaterialSymbolsFonts = paths;
+export const MaterialSymbolsFontPaths = paths;
+export const MaterialSymbolsFontPathsMap = pathsMap;
+export const MaterialSymbolsFonts = fontAssets;
 export const MaterialSymbolsGlyphMap = glyphMap;
+
+export const MaterialSymbolsOutlinedFont = fontAssets.MaterialSymbolsOutlined;
+export const MaterialSymbolsOutlinedFilledFont =
+	fontAssets.MaterialSymbolsOutlinedFilled;
+export const MaterialSymbolsRoundedFont = fontAssets.MaterialSymbolsRounded;
+export const MaterialSymbolsRoundedFilledFont =
+	fontAssets.MaterialSymbolsRoundedFilled;
+export const MaterialSymbolsSharpFont = fontAssets.MaterialSymbolsSharp;
+export const MaterialSymbolsSharpFilledFont =
+	fontAssets.MaterialSymbolsSharpFilled;
+
 export { MaterialSymbols };
