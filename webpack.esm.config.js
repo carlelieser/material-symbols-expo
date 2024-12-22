67647efe17a4089694f7cabda8b7fd1f69e85e7e
Carlos <carloseliesersantos@gmail.com>
2024-12-22 16:55:24 -0500

    remove copy-webpack-plugin

--- webpack.esm.config.js
+++ webpack.esm.config.js
@@ -2,7 +2,6 @@ import common from "./webpack.common.config.js";
 import { merge } from "webpack-merge";
 import path from "path";
 import { fileURLToPath } from "url";
-import CopyWebpackPlugin from "copy-webpack-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
