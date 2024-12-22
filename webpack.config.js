2e2f0100acb4a4073f18e8efac8646b9898e722c
Carlos <carloseliesersantos@gmail.com>
2024-12-21 23:32:38 -0500

    use webpack + babel for compilation, which would allow for import inside app.config.js

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+import path from "path";
+import { fileURLToPath } from 'url';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+	entry: "./src/index.ts",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "index.js",
+		library: "MaterialSymbols",
+		libraryTarget: "umd",
+		globalObject: "this",
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(ts|tsx|js|jsx)$/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+			{
+				test: /\.ttf/,
+				use: {
+					loader: "ignore-loader"
+				}
+			}
+		],
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js", ".jsx"],
+	},
+	mode: "production",
+	devtool: "source-map",
+	plugins: [
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, 'src/assets'),
+					to: path.resolve(__dirname, 'dist/assets'),
+				},
+			],
+		}),
+	],
+	externals: {
+		react: "react",
+		"react-dom": "react-dom",
+		"react-native": "react-native",
+	},
+};
